1e3e49ceb78f9cd1172abe0f17b40e9d47baa215
mamounyosef <mamounhussam1000@gmail.com>
2026-03-12 00:16:10 +0300

    Explore and clean and filter the dataset, and add the download_dataset script

---
+++ data/download_dataset.py
@@ -0,0 +1,22 @@
+from huggingface_hub import snapshot_download
+
+# snapshot_download(
+#     repo_id="aipieces/How2Sign",
+#     repo_type="dataset",
+#     allow_patterns="train_rgb_front_clips/*",
+#     local_dir=r"D:\How2Sign\train"
+# )
+
+snapshot_download(
+    repo_id="aipieces/How2Sign",
+    repo_type="dataset",
+    allow_patterns="val_rgb_front_clips/*",
+    local_dir=r"D:\How2Sign\val"
+)
+
+snapshot_download(
+    repo_id="aipieces/How2Sign",
+    repo_type="dataset",
+    allow_patterns="test_rgb_front_clips/*",
+    local_dir=r"D:\How2Sign\test"
+)
